d86f812e0b2ef231c7355e195fa4012149930983
Sairam pentapati <109594074+ramm1004@users.noreply.github.com>
2025-08-21 10:14:20 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,8 +1,15 @@
 terraform {
+  backend "s3" {
+    bucket = "sairam-bucket"
+    key = "sairam.tfstate"
+    region = "eu-west-3"
+    use_lockfile = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
+
 }
